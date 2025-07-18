3279083e61faea8c041035dc09889f876c5ca578
heerpatel9898 <109365132+heerpatel9898@users.noreply.github.com>
2025-07-18 21:41:48 +0530

    Add files via upload

---
+++ variables/basic.py
@@ -0,0 +1,141 @@
+# some basic variables and some equetions & sum of maths
+
+from math import floor
+
+a = "heer"
+b = 23
+c = 17
+d = 20
+e = (d+b)*c  
+f = 5.3
+g = d/b
+h = d//f
+i = d/f
+j = 5
+k = 2
+l =-5
+m =-2
+
+print(e)
+
+print(f)
+
+sum = b+c
+print(sum)
+
+diff = b-c
+print(diff)
+
+multi = b*c
+print(multi)
+
+print(d)
+print(d//f)
+print(d/f)
+floor(d//f)
+
+divide = j/k
+print(divide)
+
+divide1 = l/m
+print(divide1)
+
+divide2 = j/m
+print(divide2)
+
+divide3 = l/k
+print(divide3)
+
+A,B = -6,2
+c = A%B
+print(c)
+
+D,F = 6,-2
+G = D%F
+print(G)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(d//f))
+
+"""#Arithmetic operaters in python(+,-,*,/,%,**)
+
+a = 3
+b = 2
+sum = a%b
+print(sum)
+print(a**b)
+
+#rational operaters in python(==,!=,<,>,<=,>=)
+
+c = 4
+d = 5
+
+print("ans:",a==b)
+print("ans:",a<b)
+print("ans:",a>b)
+print("ans:",a<=b)
+print("ans:",a>=b)
+print("ans:",a!=b)
+
+#asignment operaters(=,+=,-=,*=,/=,%=,**=)
+
+e = 6
+f = 7
+
+num = 6+7
+num = num*5 # num  *= 5
+print(num)
+
+num2 = 6+7
+num2 = num2 /2 # num2 /= 2
+print(num2)
+
+num3 = 6+7
+num3 = num3%2 # num3 %= 2
+print(num3)
+
+num4 = 6+7
+num4 = num4**2 # num4 **= 2
+print(num4)
+
+#logical operaters(and,or,not)
+
+g = 3
+h = 2
+eqe = 2>3
+eqe2 = 3>2
+
+print(not (2>3))
+print(not(3>2))
+print( 2>3)
+print(3>2)
+print(eqe and eqe2)
+print(eqe or eqe2)
+print((g==h) and (g>h))
+print((g==h) or (g>h))"""
+
+#type conversion
+
+"""a = "2"
+b = 2.25
+a = float("2")
+sum = a+b
+print(type(a))
+print(sum)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables/input.py
@@ -0,0 +1,18 @@
+#input uses in python
+
+"""name = input("Name = ")
+age = input("Age = ")
+hobby = input("Hobby = ")
+
+print("welcome!",name)
+
+val = int(input("enter some value : "))
+print(type(val),val)"""
+ 
+name = str(input("enter your name :"))
+age = int(input("age :"))
+marks = float(input("enter your grades :"))
+
+print("your name is",name,".")
+print("you are",age,"year old")
+print("and your grades is",marks,".")
